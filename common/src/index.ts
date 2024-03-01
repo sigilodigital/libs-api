f2ab9acdf5fdf923ede3c11f3c05fdfe1293ab7a
Haroldo Cruz <haroldocruzdr@gmail.com>
2024-03-01 18:47:06 -0300

    refactor(common): enum

--- common/src/index.ts
+++ common/src/index.ts
@@ -2,5 +2,7 @@ export * from './common.module';
 export * from './common.service';
 
 // INTERNALS
+// export * from './repository/generic.repository'
+// export * from './repository/util.repository'
 export * from './databases/runner-transaction/runner-transaction'
 export * from './databases/'
